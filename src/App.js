86d10790c40b2a804aca3702c1dc92dac8359f3b
Francis Nwoba <frenzynwoba@gmail.com>
2022-10-08 14:11:07 +0100

    Chukwuebuka: Project Setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>A squatting platform </p>
+        <p>
+          This platform allows people to accommodate others for an agreed period
+          of time.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
