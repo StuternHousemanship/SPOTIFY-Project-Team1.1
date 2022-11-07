b96d3857e5739fd816e435238e5fac2cf0e56732
unyime <unyimeusen01@gmail.com>
2022-11-07 22:05:55 +0100

    Unyime T3: SZL-1 Onboarding Features V22

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { NonAuthRoutes } from "../url";
 
 function Dashboard() {
   const navigate = useNavigate();
-  const logOut = () => {
-    navigate("/login");
+  const logout = () => {
+    navigate(NonAuthRoutes.login);
   };
 
   return (
     <main className="min-h-screen">
       <div className="flex justify-end p-4 bg-squazzle-white-background-color">
         <button
-          onClick={logOut}
+          onClick={() => logout()}
           type="button"
           className="bg-squazzle-grey-text-color py-2 px-3 rounded-md text-squazzle-white-background-color"
         >
