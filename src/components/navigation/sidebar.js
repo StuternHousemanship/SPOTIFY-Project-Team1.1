88e27e2e2dd0413c71f6104645d76109483de6fd
unyime <unyimeusen01@gmail.com>
2022-12-07 08:49:48 +0100

    Unyime T4: SZL-2 Dashboard Features V76

--- src/components/navigation/sidebar.js
+++ src/components/navigation/sidebar.js
@@ -122,7 +122,7 @@ const sidebar = () => {
           <li className="w-full">
             <hr className="border border-squazzle-border-grey-color" />
           </li>
-          <li className="text-squazzle-profileCard-logout-red-color">
+          <li className="text-squazzle-profileCard-logout-red-color mb-[300px]">
             <button type="button" onClick={() => handleLogout()}>
               Logout
             </button>
